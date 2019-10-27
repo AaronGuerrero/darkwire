fdf46c1d34190111054d37c641f4339db1de08de
AaronGuerrero <aaronwiggy@gmail.com>
2019-10-27 16:51:48 -0700

    Add files via upload

---
+++ client/src/components/chat/Chat.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import { Chat } from './index.js'
+
+const sendEncryptedMessage = jest.fn()
+
+test('Chat Component', () => {
+  const component = mount(
+    <Chat scrollToBottom={() => {}} focusChat={false} userId="foo" username="user" showNotice={() => {}} clearActivities={() => {}} sendEncryptedMessage={sendEncryptedMessage} />
+  )
+
+  const componentJSON = toJson(component)
+
+  expect(component).toMatchSnapshot()
+  expect(componentJSON.children.length).toBe(1)
+})
