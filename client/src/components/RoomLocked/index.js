55ad8c3fa1e4b2d6ea043977faf539885403c543
AaronGuerrero <aaronwiggy@gmail.com>
2019-10-27 16:58:57 -0700

    Add files via upload

---
+++ client/src/components/RoomLocked/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class RoomLocked extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.modalContent}
+      </div>
+    )
+  }
+}
